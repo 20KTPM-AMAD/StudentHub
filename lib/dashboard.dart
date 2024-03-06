96e33002077c4e492bb01079d3f11ddcc465b447
Lam Ha <lam.ha@devsamurai.com>
2024-03-06 20:13:33 +0700

    add dashboard, welcom screen and edit profile with mock UI

---
+++ lib/dashboard.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+const Color _green = Color(0xFF12B28C);
+
+class Dashboard extends StatefulWidget {
+  const Dashboard({Key? key}) : super(key: key);
+
+  @override
+  DashboardState createState() => DashboardState();
+}
+
+class DashboardState extends State<Dashboard> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('StudentHub'),
+        backgroundColor: _green,
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.person),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                    child: Row(
+                      children: [
+                        const Text('Your jobs'),
+                        const Spacer(),
+                        ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                              foregroundColor: Colors.black,
+                              backgroundColor: _green,
+                              fixedSize: const Size(120, 24),
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(4))),
+                          child: const Text('Post a job',
+                              style: TextStyle(fontSize: 14)),
+                        )
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  const Text(
+                    'Welcome, Hai!',
+                    style: TextStyle(
+                      fontSize: 14,
+                    ),
+                  ),
+                  const Text(
+                    'You have no jobs!',
+                    style: TextStyle(
+                      fontSize: 14,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: NavigationBar(
+        height: 60,
+        destinations: const [
+          NavigationDestination(icon: Icon(Icons.view_list), label: 'Projects'),
+          NavigationDestination(
+            icon: Icon(Icons.home),
+            label: 'Dashboard',
+          ),
+          NavigationDestination(icon: Icon(Icons.message), label: 'Message'),
+          NavigationDestination(
+            icon: Icon(Icons.notifications),
+            label: 'Alerts',
+          ),
+        ],
+        backgroundColor: _green,
+      ),
+    );
+  }
+}
---
+++ lib/pages/profile/profile_edit_page.dart
@@ -0,0 +1,191 @@
+import 'package:flutter/material.dart';
+
+const Color _green = Color(0xFF12B28C);
+
+enum Choices { justMe }
+
+class ProfileEditPage extends StatefulWidget {
+  const ProfileEditPage({Key? key}) : super(key: key);
+
+  @override
+  ProfileEditPageState createState() => ProfileEditPageState();
+}
+
+class ProfileEditPageState extends State<ProfileEditPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('StudentHub'),
+        backgroundColor: _green,
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.person),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+              child: Column(
+                children: [
+                  const SizedBox(height: 30),
+                  const Text(
+                    'Welcome to Student Hub',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  SizedBox(
+                    width: 350,
+                    child: Column(
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              'Company name',
+                              style: TextStyle(
+                                fontSize: 16,
+                              ),
+                            ),
+                            const SizedBox(height: 8),
+                            TextField(
+                              decoration: InputDecoration(
+                                contentPadding: const EdgeInsets.symmetric(
+                                    vertical: 10.0, horizontal: 15.0),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                  borderSide: const BorderSide(color: _green),
+                                ),
+                                hintText: "Enter company name",
+                                hintStyle: const TextStyle(color: _green),
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 20),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              'Website',
+                              style: TextStyle(
+                                fontSize: 16,
+                              ),
+                            ),
+                            const SizedBox(height: 8),
+                            TextField(
+                              decoration: InputDecoration(
+                                contentPadding: const EdgeInsets.symmetric(
+                                    vertical: 10.0, horizontal: 15.0),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                  borderSide: const BorderSide(color: _green),
+                                ),
+                                hintText: "Enter web site",
+                                hintStyle: const TextStyle(color: _green),
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 20),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              'Description',
+                              style: TextStyle(
+                                fontSize: 16,
+                              ),
+                            ),
+                            TextField(
+                              maxLines: 6, //or null
+                              decoration: InputDecoration(
+                                contentPadding: const EdgeInsets.symmetric(
+                                    vertical: 10.0, horizontal: 15.0),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                  borderSide: const BorderSide(color: _green),
+                                ),
+                                hintText: "Description...",
+                                hintStyle: const TextStyle(color: _green),
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 20),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              'How many people are in your company?',
+                              style: TextStyle(
+                                fontSize: 16,
+                              ),
+                            ),
+                            ListTile(
+                              title: const Text('Just me'),
+                              leading: Radio<Choices>(
+                                value: Choices.justMe,
+                                groupValue: Choices.justMe,
+                                onChanged: (Choices? value) {},
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Row(
+                    children: [
+                      const Spacer(),
+                      ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                            foregroundColor: Colors.black,
+                            backgroundColor: _green,
+                            fixedSize: const Size(120, 32),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(4))),
+                        child:
+                            const Text('Edit', style: TextStyle(fontSize: 16)),
+                      ),
+                      const SizedBox(width: 20),
+                      ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                            foregroundColor: Colors.black,
+                            backgroundColor: Colors.grey[300],
+                            fixedSize: const Size(120, 32),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(4))),
+                        child: const Text('Cancel',
+                            style: TextStyle(fontSize: 16)),
+                      ),
+                      const SizedBox(width: 20),
+                    ],
+                  )),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: const Padding(
+        padding: EdgeInsets.only(bottom: 20),
+        child: SizedBox(),
+      ),
+    );
+  }
+}
