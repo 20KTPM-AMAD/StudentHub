b2474a6fecee578dd23d4c94018e4510f8ba8f56
LeThuyNga1606 <lethuynga1662002@gmail.com>
2024-03-26 17:31:27 +0700

    add lazy loading

--- lib/components/company_project/card_project_list.dart
+++ lib/components/company_project/card_project_list.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:studenthub/pages/browse_project/project_detail_screen.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:studenthub/pages/browse_project/project_detail_screen.dart';
 
 const Color _green = Color(0xFF12B28C);
 
@@ -12,6 +12,11 @@ class ProjectListTab extends StatefulWidget {
 }
 
 class ProjectListTabState extends State<ProjectListTab> {
+  bool isLoading = false;
+  int currentPage = 0;
+  int itemsPerPage = 10;
+  List<String> items = List.generate(10, (index) => 'Item $index');
+
   @override
   void initState() {
     super.initState();
@@ -19,94 +24,140 @@ class ProjectListTabState extends State<ProjectListTab> {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-      shrinkWrap: true,
-      physics: const ScrollPhysics(),
-      separatorBuilder: (BuildContext context, int index) => const SizedBox(height: 10),
-      itemCount: 10,
-      itemBuilder: (context, index) {
-        return Card(
-          margin: const EdgeInsets.all(5.0),
-          child: ListTile(
-            title: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
+    return NotificationListener<ScrollNotification>(
+      onNotification: (ScrollNotification scrollInfo) {
+        if (!isLoading &&
+            scrollInfo.metrics.pixels == scrollInfo.metrics.maxScrollExtent) {
+          _loadMoreItems();
+        }
+        return true;
+      },
+      child: ListView.separated(
+        shrinkWrap: true,
+        physics: const ScrollPhysics(),
+        separatorBuilder: (BuildContext context, int index) =>
+        const SizedBox(height: 10),
+        itemCount: items.length + 1,
+        itemBuilder: (context, index) {
+          if (index == items.length) {
+            return _buildProgressIndicator();
+          } else {
+            return Card(
+              margin: const EdgeInsets.all(5.0),
+              child: ListTile(
+                title: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    const Expanded(
-                      child: Text(
-                        'Senior frontend developer (Fintech)',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                          color: _green,
+                    Row(
+                      children: [
+                        const Expanded(
+                          child: Text(
+                            'Senior frontend developer (Fintech)',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                              color: _green,
+                            ),
+                            overflow: TextOverflow.ellipsis,
+                          ),
                         ),
-                        overflow: TextOverflow.ellipsis, // Hiển thị dấu ... khi văn bản tràn ra ngoài
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.favorite_border_outlined,
+                            size: 30,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Text(
+                      AppLocalizations.of(context)!.time_created_project('3'),
+                      style: const TextStyle(
+                        fontStyle: FontStyle.italic,
+                        color: Colors.grey,
                       ),
                     ),
-                    const SizedBox(width: 10,),
-                    IconButton(
-                      onPressed: () {
-
-                      },
-                      icon: const Icon(Icons.favorite_border_outlined, size: 30,),
+                    Text(
+                      AppLocalizations.of(context)!.time_needed_project('1-3') +
+                          AppLocalizations.of(context)!
+                              .student_needed_project('6'),
+                      style: const TextStyle(
+                        fontStyle: FontStyle.italic,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    RichText(
+                      text: const TextSpan(
+                        style: TextStyle(color: Colors.black),
+                        children: [
+                          TextSpan(
+                            text: 'Students are looking for:\n',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          TextSpan(
+                            text:
+                            '- Clear expectation about your project or deliverables',
+                            style: TextStyle(
+                              fontSize: 16,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      '${AppLocalizations.of(context)!.proposals}: Less than 5',
+                      style: const TextStyle(
+                        fontStyle: FontStyle.italic,
+                        color: Colors.grey,
+                      ),
                     ),
                   ],
                 ),
-
-                Text(
-                  AppLocalizations.of(context)!.time_created_project('3'),
-                  style: const TextStyle(
-                    fontStyle: FontStyle.italic,
-                    color: Colors.grey,
-                  ),
-                ),
-                Text(
-                  AppLocalizations.of(context)!.time_needed_project('1-3') + AppLocalizations.of(context)!.student_needed_project('6'),
-                  style: const TextStyle(
-                    fontStyle: FontStyle.italic,
-                    color: Colors.grey,
-                  ),
-                ),
-                const SizedBox(height: 10),
-                RichText(
-                  text: const TextSpan(
-                    style: TextStyle(color: Colors.black),
-                    children: [
-                      TextSpan(
-                        text: 'Students are looking for:\n',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      TextSpan(
-                        text: '- Clear expectation about your project or deliverables',
-                        style: TextStyle(
-                          fontSize: 16, // Cỡ chữ
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(height: 10),
-                Text(
-                  '${AppLocalizations.of(context)!.proposals}: Less than 5',
-                  style: const TextStyle(
-                    fontStyle: FontStyle.italic,
-                    color: Colors.grey,
-                  ),
-                ),
-              ],
-            ),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const ProjectDetailScreen()),
-              );
-            },
-          ),
-        );
-      },
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const ProjectDetailScreen()),
+                  );
+                },
+              ),
+            );
+          }
+        },
+      ),
     );
+  }
+
+  Widget _buildProgressIndicator() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+
+  void _loadMoreItems() {
+    setState(() {
+      isLoading = true;
+    });
+
+    // Simulate a delay for fetching new items
+    Future.delayed(Duration(seconds: 2), () {
+      setState(() {
+        int currentLength = items.length;
+        for (int i = currentLength; i < currentLength + itemsPerPage; i++) {
+          items.add('Item $i');
+        }
+        isLoading = false;
+      });
+    });
   }
 }
